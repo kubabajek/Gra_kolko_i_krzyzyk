01a23d63f69a7c13f56ad2d2c287f5d0c811bff0
Jakub Bajek <81303114+kubabajek@users.noreply.github.com>
2021-05-13 23:03:14 +0200

    Update main.py

--- Szkielet - zaczynam robić/main.py
+++ Szkielet - zaczynam robić/main.py
@@ -14,3 +14,19 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def print_tic_tac_toe(values): #Plansza
+    print("\n")
+    print("\t     |     |")
+    print("\t  {}  |  {}  |  {}".format(values[0], values[1], values[2]))
+    print('\t_____|_____|_____')
+
+    print("\t     |     |")
+    print("\t  {}  |  {}  |  {}".format(values[3], values[4], values[5]))
+    print('\t_____|_____|_____')
+
+    print("\t     |     |")
+
+    print("\t  {}  |  {}  |  {}".format(values[6], values[7], values[8]))
+    print("\t     |     |")
+    print("\n")

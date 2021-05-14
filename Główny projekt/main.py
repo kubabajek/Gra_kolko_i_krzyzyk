54362ce91799dc5c24e5a8e3257d00bf748c4d46
Jakub Bajek <81303114+kubabajek@users.noreply.github.com>
2021-05-14 12:33:52 +0200

    Update main.py

--- Główny projekt/main.py
+++ Główny projekt/main.py
@@ -1,16 +1,24 @@
 import time
 import basicnobot
+def Zliczanie():
+    global uruchomien
+    global ulubionytryb
+    global uruchomientryb11
+    uruchomien = 1
+    uruchomientryb11 = 0
+def Zapis_zliczen():
+    print()
 def Wstep () :
     print ('\n###################################\nProfesjonalna gra w kolko i krzyzyk\n###################################')
-    print ('Uruchomien: ')
+    print ('Uruchomien:',uruchomien)
     print ('Lacznie zagranych partii: ')
-    print ('Ulubiony tryb gry: ', end="")
-    print ('Przykladowy tryb gry') #DO ZMIANY!!!!!!!!!!!!
+    print ('Ulubiony tryb gry: ',)
     print ('###################################')
     print ('Loading...')
     time.sleep(5)
 def main() :
-    Wstep() #Mozna zakomentowac zeby nie czekac
+    Zliczanie()
+    Wstep()
     tryb =1
     while (tryb != 0) :
         tryb = int(input('\n\n\n\n\n\n\nWybierz tryb gry:\n1 - Gra na planszy 3x3 \n2 - \n0 - Wyjscie\n'))
@@ -23,7 +31,7 @@ def main() :
                 basicnobot.main()
             elif (tryb2 == 2) :
                 print ('Gra z botem niezaimplementowana') #3x3 GRA Z KOMPUTEREM TO DO
-    #zapis do pliku
+    Zapis_zliczen()
     print ('Zegnaj')
     exit(0)
 if __name__ == '__main__' :

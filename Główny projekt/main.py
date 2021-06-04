2222b2b968410575c68b5afcf9a85da5bbabb389
Jakub Bajek <81303114+kubabajek@users.noreply.github.com>
2021-06-04 14:03:07 +0200

    Swoje zrobiłem
    Uporządkowałem kod i przystosowałem maina do dodania brakujących funkcji. Grę na czas dodajcie w plikach z dopiskiem time, macie pusty plik tekstowy na statystyki nawet XD

--- Główny projekt/main.py
+++ Główny projekt/main.py
@@ -1,115 +1,131 @@
 import time
 import os
-import basicnobot
-import nobot4x4
-import nobot5x5
-import bot3x3
-def Zliczanie():
+import game3x3
+import game3x3bot
+import game3x3time
+import game3x3timebot
+import game4x4
+import game5x5
+
+def zliczanie():
     global ulubionytryb #nie ma go w pliku
     global zagran # nie ma go w pliku
     global uruchomien
     global t11
     global t12
-    global t21
-    global t22
-    global t31
-    global t32
+    global t13
+    global t14
+    global t20
+    global t30
     ulubionytryb = 'Brak, pokaze sie wkrotce =]'
-    if os.path.exists('zliczenia.txt') == False:
+    if not os.path.exists('zliczenia.txt'):
         plikzliczenia = open('zliczenia.txt','w') #STRUKTURA PLIKU: uruchomien, kolejnetryby11, 12, 13, 21, 22 ... wszystko w innych wierszach
-        plikzliczenia.write("1\n0\n0\n0\n0\n0\n0")
+        plikzliczenia.write("0\n0\n0\n0\n0\n0\n0")
         plikzliczenia.close()
     plikzliczenia = open('zliczenia.txt', 'r')
     linie=plikzliczenia.readlines()
     plikzliczenia.close()
-    uruchomien=int(linie[0])
+    uruchomien = int(linie[0])
     t11=int(linie[1])
     t12=int(linie[2])
-    t21=int(linie[3])
-    t22=int(linie[4])
-    t31=int(linie[5])
-    t32=int(linie[6])
+    t13=int(linie[3])
+    t14=int(linie[4])
+    t20=int(linie[5])
+    t30=int(linie[6])
+    uruchomien += 1
+    zapis_zliczen()
 
 #Ustalanie ulubionego trybu gry
-    if (t11 > max(t12,t21,t22,t31,t32)) :
-        ulubionytryb = '3x3 z kolega'
-    if (t12 > max(t11,t21, t22,t31,t32)) :
+    if t11 > max(t12, t13, t14, t20, t30):
+        ulubionytryb = '3x3 z przyjacielem'
+    if t12 > max(t11, t13, t14, t20, t30):
         ulubionytryb = '3x3 z komputerem'
-    if (t21 > max(t11, t12, t22,t31,t32)) :
-        ulubionytryb = '4x4 z kolega'
-    if (t22 > max(t11, t12, t21,t31,t32)) :
-        ulubionytryb = '4x4 z komputerem'
-    if (t31 > max(t11, t12, t21,t22,t32)) :
-        ulubionytryb = '5x5 z kolega'
-    if (t32 > max(t11, t12, t21,t22,t31)) :
-        ulubionytryb = '5x5 z komputerem'
-    zagran = t11 + t12 + t21 + t22 + t31 + t32
-def Zapis_zliczen():
+    if t13 > max(t11, t12, t14, t20, t30):
+        ulubionytryb = '3x3 z przyjacielem na czas'
+    if t14 > max(t11, t12, t13, t20, t30):
+        ulubionytryb = '3x3 z komputerem na czas'
+    if t20 > max(t11, t12, t13, t14, t30):
+        ulubionytryb = '4x4 z przyjacielem'
+    if t30 > max(t11, t12, t13, t14, t20):
+        ulubionytryb = '5x5 z przyjacielem'
+    zagran = t11 + t12 + t13 + t14 + t20 + t30
+
+
+def zapis_zliczen():
     plikzliczenia = open('zliczenia.txt', 'w')
-    plikzliczenia.write(str(uruchomien+1))
+    plikzliczenia.write(str(uruchomien))
     plikzliczenia.write("\n")
     plikzliczenia.write(str(t11))
     plikzliczenia.write("\n")
     plikzliczenia.write(str(t12))
     plikzliczenia.write("\n")
-    plikzliczenia.write(str(t21))
+    plikzliczenia.write(str(t13))
     plikzliczenia.write("\n")
-    plikzliczenia.write(str(t22))
+    plikzliczenia.write(str(t14))
     plikzliczenia.write("\n")
-    plikzliczenia.write(str(t31))
+    plikzliczenia.write(str(t20))
     plikzliczenia.write("\n")
-    plikzliczenia.write(str(t32))
+    plikzliczenia.write(str(t30))
     plikzliczenia.close()
-def Wstep () :
+
+
+def wstep():
     print ('\n###################################\nProfesjonalna gra w kolko i krzyzyk\n###################################')
-    print ('Uruchomien:',uruchomien)
-    print ('Lacznie zagranych partii:',zagran)
-    print ('Ulubiony tryb gry:',ulubionytryb)
+    if uruchomien == 10:
+        print ('TO JUZ DZIESIATE URUCHOMIENIE =]')
+    elif uruchomien == 100:
+        print ('SETNE URUCHOMIENIE, MUSI CI SIE PODOBAC <3')
+    elif uruchomien == 1000:
+        print ('TYSIECZNE URUCHOMIENIE, TO JUZ BYCIE NERDEM XD')
+    else:
+        print ('Uruchomien:', uruchomien)
+    print ('Lacznie zagranych partii:', zagran)
+    print ('Ulubiony tryb gry:', ulubionytryb)
     print ('###################################')
     print ('Loading...')
     time.sleep(5)
     print("\n\n\n\n\n\n\n\n")
+
+
 def main() :
-    Zliczanie()
-    Wstep()
-    tryb =1
-    while (tryb != 0) :
+    zliczanie()
+    wstep()
+    tryb = 1 #dowolna wartosc rozna od zera zeby ponizsza petla sie wykonywala
+    while tryb != 0 :
         tryb = int(input('\nWybierz tryb gry:\n1 - Gra na planszy 3x3 dodatkowe funkcjonalnosci \n2 - Gra na planszy 4x4 w dwie osoby\n3 - Gra na planszy 5x5 w dwie osoby\n0 - Wyjscie\n'))
-        if (tryb == 1) :
-            tryb1 = int (input('Wybrales gre 3x3, wybierz rodzaj\n1 - Z kolega \n2 - Z komputerem \n 3 - Z kolega na czas\n4 - Z komputerem na czas\n0 - Cofnij\n'))
-            if (tryb1 == 0) :
+        if tryb == 1 :
+            tryb1 = int (input('Wybrales gre 3x3, wybierz rodzaj\n1 - Z kolega \n2 - Z komputerem \n3 - Z kolega na czas\n4 - Z komputerem na czas\n0 - Cofnij\n'))
+            if tryb1 == 0 :
                 print ("Cofam...")
-            elif (tryb1 == 1) :
-                print ('\nRozpoczynam gre na planszy 3x3 bez bota\n')
+            elif tryb1 == 1 :
+                print ('\nRozpoczynam gre na planszy 3x3 z przyjacielem\n')
                 global t11
-                t11+=basicnobot.main()
-                Zapis_zliczen()
-            elif (tryb1 == 2) :
-                print ('Rozpoczynam gre na planszy 3x3 z botem')
+                t11 += game3x3.main()
+                zapis_zliczen()
+            elif tryb1 == 2:
+                print ('Rozpoczynam gre na planszy 3x3 z komputerem')
                 global t12
-                t12+=bot3x3.main()
-                Zapis_zliczen()
-            elif (tryb1 == 3) :
-                print ('Rozpoczynam gre na planszy 3x3 bez bota na czas')
+                t12 += game3x3bot.main()
+                zapis_zliczen()
+            elif tryb1 == 3:
+                print ('Rozpoczynam gre na planszy 3x3 z przyjacielem na czas')
                 global t13
-                t13+=bot3x3.main() #BEZ BOTA NA CZAS 3x3 NAZWA PLIKU DO DODANIA!!!!!!!
-                Zapis_zliczen()
-            elif (tryb1 == 4) :
-                print ('Rozpoczynam gre na planszy 3x3 z botem na czas')
+                t13 += game3x3time.main()
+                zapis_zliczen()
+            elif tryb1 == 4 :
+                print ('Rozpoczynam gre na planszy 3x3 z komputerem na czas')
                 global t14
-                t14+=bot3x3.main() #Z BOTEM NA CZAS 3x3 NAZWA PLIKU DO DODANIA!!!!
-                Zapis_zliczen()
-        if (tryb == 2) :
+                t14 += game3x3timebot.main()
+                zapis_zliczen()
+        if tryb == 2 :
             print ('\nRozpoczynam gre na planszy 4x4\n')
-            global t21
-            t21+= nobot4x4.main()
-
-        if (tryb == 3) :
+            global t20
+            t20 += game4x4.main()
+        if tryb == 3:
             print ('\nRozpoczynam gre na planszy 5x5\n')
-            global t31
-            t31+= nobot5x5.main()
-            Zapis_zliczen()
-
+            global t30
+            t30 += game5x5.main()
+            zapis_zliczen()
     print ('Zegnaj')
     exit(0)
 
